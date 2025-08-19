6ae2a08378bc4a7c586b7bd0dd008f9326c9c259
WongYC-66 <yengchien@hotmail.com>
2025-08-19 13:59:36 +0800

    add : index.js (DRAFT)

---
+++ index.js
@@ -0,0 +1,20 @@
+// todo
+const scrapData = () => {
+
+}
+
+
+// todo
+const writeToFile = (data) => {
+    // data as Array of JSON
+}
+
+
+
+//  main function
+const main = () => {
+    // todo: scrap data and store it as arr
+    // todo: File IO output to data.json
+}
+
+main()
