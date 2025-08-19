3d26af5ce320a01cf1787b9013794811f9c77cb5
WongYC-66 <yengchien@hotmail.com>
2025-08-19 14:47:45 +0800

    idea : 2 alternatives to scrap

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import { writeFile } from 'node:fs/promises';
 
 // todo
-export const scrapData = () => {
+export const scrapData = async () => {
     // technicality issue :
     // reddit dont use pagination like ?page=1
     // has to find other way
+    // option 1 - reddit-api, https://developers.reddit.com/docs/quickstart
+    // option 2 - headless browser agent like playwright - troublesome, last resort
+
+    return []
 }
 
 
@@ -12,15 +16,19 @@ export const scrapData = () => {
 export const writeToFile = async (data, filename) => {
     // data as Array of JSON
     const stringified = JSON.stringify(data)
-    await  writeFile(filename, stringified, { encoding: "utf-8" });
+    await writeFile(filename, stringified, { encoding: "utf-8" });
 }
 
 
 
 //  main function
-const main = () => {
+const main = async () => {
     // todo: scrap data and store it as arr
+
+    const data = await scrapData()
+
     // todo: File IO output to data.json
+    await writeToFile(data, "data.json")
 }
 
 main()
