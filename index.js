b252534842a433b68481740d4687fed022f7abbc
WongYC-66 <yengchien@hotmail.com>
2025-08-19 17:58:49 +0800

    scale: to 10 pages

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-import { error } from 'node:console';
 import { writeFile } from 'node:fs/promises';
 
 // const FETCH_TIMES = 10
-const FETCH_TIMES = 3
+const FETCH_TIMES = 10
 
 export const scrapData = async () => {
-    // technicality issue :
-    // reddit dont use pagination like ?page=1 , has to find other way
-    // REJECTED - option 1 - reddit-api, https://developers.reddit.com/docs/quickstart
-    // option 2 - headless browser agent like playwright - troublesome, last resort
-    // option 3 - reddit's .rss/json ? https://stackoverflow.com/questions/31942743/how-to-get-json-data-from-reddit-after-the-first-page
-
-    // let's try option 3. Reddit .json
+    // Approach - reddit's .json
+    // https://stackoverflow.com/questions/31942743/how-to-get-json-data-from-reddit-after-the-first-page
 
     let after = null
     let allData = []
 
     // fetch remaining 10 times
-    // Array(FETCH_TIMES).fill().forEach(async() => {
     for (let i = 0; i < FETCH_TIMES; i++) {
         // use sychonous/blocking method, to respect rate-limiter of Reddit
         const data = await fetchReddit({ after });
@@ -34,10 +27,9 @@ export const scrapData = async () => {
 // helper fn
 const fetchReddit = async (options) => {
     const { after = "null" } = options  // default to null
-
     // https://www.reddit.com/r/redditdev/comments/1866kxm/cannot_view_reddit_images_from_json_api/
-    // append raw_json=1 to avoid reddit encrpytion
-    const fetchURL = `https://www.reddit.com/r/malaysia/new/.json?after=${after}&raw_json=1`
+    
+    const fetchURL = `https://www.reddit.com/r/malaysia/new/.json?after=${after}&raw_json=1`     // append raw_json=1 to avoid reddit encrpytion
     console.log({ fetchURL })
     try {
         const response = await fetch(fetchURL)
@@ -79,19 +71,17 @@ const processData = (data) => {
 
 
 export const writeToFile = async (data, filename) => {
-    // data as Array of JSON
+    // data as Array of JS Object
     const stringified = JSON.stringify(data, null, 2)
     await writeFile(filename, stringified, { encoding: "utf-8" });
 }
 
 
 const main = async () => {
-
     const data = await scrapData()
     const processedData = processData(data)
     const filteredData = removeDataWithoutMedia(processedData)
     await writeToFile(filteredData, "data.json")
-    // await writeToFile(filteredData, "data.json")
 }
 
 main()
